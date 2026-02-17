31074e62e863c533c27e1169fdf2fe9c40a50902
Thaís Moreira <thaisdmedeiros@gmail.com>
2026-02-17 12:01:04 +0100

    tests: update user_repository_tests to unit tests with mock

--- src/models/repositories/user_repository_test.py
+++ src/models/repositories/user_repository_test.py
@@ -1,72 +1,108 @@
 from .user_repository import UserRepository
 from src.models.settings.db_connection_handler import db_connection_handler
+from unittest.mock import Mock
 
-# separei o teste do professor em 3 testes:
-
-
-def test_registry_user():
-    db_connection_handler.connect()
-    connection = db_connection_handler.get_connection()
-    repository = UserRepository(connection)
-
-    username = "Patrick"
-    password = "124Rocket"
-
-    # criando registro
-    repository.registry_user(username, password)
+# transformar em testes unitarios
 
 
-def test_get_user_by_username():
-    db_connection_handler.connect()
-    connection = db_connection_handler.get_connection()
-    repository = UserRepository(connection)
-
-    # pegando as informações do user
-    user = repository.get_user_by_username("Patrick")
-    print()
-    print(user)
+class MockCursor:
+    def __init__(self) -> None:
+        self.execute = Mock()
+        self.fetchone = Mock()
 
 
-def test_edit_balance():
-    db_connection_handler.connect()
-    connection = db_connection_handler.get_connection()
-    repository = UserRepository(connection)
-
-    # pegando as informações do user
-    user = repository.get_user_by_username("Patrick")
-    print()
-    print(user)
-
-    # editando o balance -
-    #  só aparece no print se editar get_user_by_username para mostrar esse dado:
-    #  SELECT id, username, password, balance
-    repository.edit_balance(user[0], 6672.10)
-    user2 = repository.get_user_by_username("Patrick")
-    print()
-    print(user2)
+class MockConnection:
+    def __init__(self) -> None:
+        self.cursor = Mock(return_valule=MockCursor())
+        self.commit = Mock()
 
 
-# esse foi o teste que o professor fez, ele fez tudo junto,
-# depois mudou só para get_user_by_username  para não ficar sempre inserindo dados no banco
+class TestRepository:
 
+    def test_registry_user(self):
+        # Cria uma conexão falsa (mockada)
+        mock_connection = MockConnection()
 
-def test_repository():
-    db_connection_handler.connect()
-    connection = db_connection_handler.get_connection()
-    repository = UserRepository(connection)
+        # Injeta essa conexão falsa no repositório
+        repository = UserRepository(mock_connection)
 
-    username = "Bob Esponja"
-    password = "123Rocket"
+        username = "Fred"
+        password = "Yabadabadooo"
 
-    # criando registro
-    repository.registry_user(username, password)
+        # Executa o método que queremos testar - cria registro no db
+        repository.registry_user(username, password)
 
-    # pegando as informações do user
-    user = repository.get_user_by_username(username)
-    print()
-    print(user)
+        # Recupera o cursor que foi retornado pelo mock
+        cursor = mock_connection.cursor.return_value
+        # print()
+        # print(cursor) # retorna um objeto <Mock name='mock()' id='4415705808'>
+        # print(cursor.execute.call_args[0])
+        # # output:
+        # # ('\n            INSERT INTO users\n                (username, password, balance)\n
+        # # VALUES\n                (?, ?, ?)\n            ', ('Fred', 'Yabadabadooo', 0))
 
-    repository.edit_balance(user[0], 6672.10)
-    user2 = repository.get_user_by_username(username)
-    print()
-    print(user2)
+        # cursor.execute.call_args guarda os argumentos da última chamada do execute
+        # call_args[0] -> argumentos posicionais
+        # call_args[0][0] -> SQL (string)
+        # call_args[0][1] -> parâmetros (tupla)
+
+        # Verifica se o SQL contém as partes esperadas
+        assert "INSERT INTO users" in cursor.execute.call_args[0][0]
+        assert "(username, password, balance)" in cursor.execute.call_args[0][0]
+        assert "VALUES" in cursor.execute.call_args[0][0]
+
+        # Verifica se os valores enviados estão corretos
+        # O saldo inicial deve ser 0
+        assert cursor.execute.call_args[0][1] == (username, password, 0)
+
+    def test_edit_balance(self):
+        # Cria uma conexão falsa (mockada)
+        mock_connection = MockConnection()
+
+        # Injeta essa conexão falsa no repositório
+        repository = UserRepository(mock_connection)
+
+        user_id = 234
+        new_balance = 100.11
+
+        # Executa o método que queremos testar - editar o balance do db
+        repository.edit_balance(user_id, new_balance)
+
+        # Recupera o cursor que foi retornado pelo mock
+        cursor = mock_connection.cursor.return_value
+
+        # Verifica se o SQL contém as partes esperadas
+        assert "UPDATE users" in cursor.execute.call_args[0][0]
+        assert "SET balance = ?" in cursor.execute.call_args[0][0]
+        assert "WHERE id = ?" in cursor.execute.call_args[0][0]
+
+        # Verifica se os valores enviados estão corretos
+        # O saldo inicial deve ser 0
+        assert cursor.execute.call_args[0][1] == (new_balance, user_id)
+        mock_connection.commit.assert_called_once()
+
+    def test_get_user_by_username(self):
+        # Cria uma conexão falsa (mockada)
+        mock_connection = MockConnection()
+
+        # Injeta essa conexão falsa no repositório
+        repository = UserRepository(mock_connection)
+
+        username = "meuUsername"
+
+        # Executa o método que queremos testar - editar o balance do db
+        repository.get_user_by_username(username)
+
+        # Recupera o cursor que foi retornado pelo mock
+        cursor = mock_connection.cursor.return_value
+
+        # Verifica se o SQL contém as partes esperadas
+        assert "SELECT id, username, password" in cursor.execute.call_args[0][0]
+        assert "FROM users" in cursor.execute.call_args[0][0]
+        assert "WHERE username = ?" in cursor.execute.call_args[0][0]
+
+        # Verifica se os valores enviados estão corretos
+        # O saldo inicial deve ser 0
+        assert cursor.execute.call_args[0][1] == (username,)
+
+        cursor.fetchone.assert_called_once()
