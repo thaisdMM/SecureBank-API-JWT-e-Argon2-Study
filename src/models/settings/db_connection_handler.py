0cf7091c4ff0033bcd1540f07bf70fb4cb1e9f3e
Thaís Moreira <thaisdmedeiros@gmail.com>
2026-02-17 10:02:47 +0100

    feat: create handler to connect to database

---
+++ src/models/settings/db_connection_handler.py
@@ -0,0 +1,20 @@
+import sqlite3
+from sqlite3 import Connection
+
+
+# Coloquou os 2__ para esconder a classe e não importar ela diretamente, sim o objeto
+class __DbConnectionHandler:
+    def __init__(self) -> None:
+        self._connection_string = "storage.db"
+        self._connection = None
+
+    def connect(self) -> None:
+        self._connection = sqlite3.connect(self._connection_string)
+
+    def get_connection(self) -> Connection:
+        return self._connection
+
+
+# objeto para ter uma conexão fixa no banco de dados
+# vai utilizar esse objeto no restante do projeto
+db_connection_handler = __DbConnectionHandler()
